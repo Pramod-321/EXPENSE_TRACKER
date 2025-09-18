f263b1a9c4ff8b6d6df7e7465b6ca96c3ca6ecf7
pramod-321 <pramodmore7102005@gmail.com>
2025-09-18 10:48:07 +0530

    expense tracker final commit

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    
-    path('',views.index,name='index'),
+    path('',views.main,name='main'),
+    path('index/',views.index,name='index'),
     path('delete_transaction/<id>/',views.delete_transaction,name='delete_transaction'),
-    path('main/',views.main,name='main'),
+    path('login/',views.login_view,name='login_view'),
+    path('register/',views.register_view,name='register_view'),
 ]
